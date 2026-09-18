37be0f4935b17dc0affc6d285ef79ed12d25ee93
Qingsong Hu <kingsanhu@gmail.com>
2026-09-18 23:19:58 +0800

    Finish week 1, part 2: Rust Warmup

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -7,15 +7,29 @@ fn main() {
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut res: Vec<i32> = Vec::new();
+    for i in v.iter() {
+        res.push(i + n);
+    }
+    res
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for i in v.iter_mut() {
+        *i += n;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut set = HashSet::new();
+    let mut dedup_vec = Vec::new();
+    for i in v.iter() {
+        if !set.contains(i) {
+            dedup_vec.push(*i);
+        }
+        set.insert(*i);
+    }
+    *v = dedup_vec;
 }
 
 #[cfg(test)]
